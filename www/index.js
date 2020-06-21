3bdce08ee2ed08feeb14cce9d35717ed4d6e6d4e
Fabian Zachs <fabianzachs@Fabians-MacBook-Pro.local>
2020-06-21 16:12:28 +0100

    version 1

--- www/index.js
+++ www/index.js
@@ -1,3 +1,17 @@
-import * as wasm from "wasm-game-of-life";
+import { Universe } from "wasm-game-of-life";
 
-wasm.greet("Fabian");
+
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+  pre.textContent = universe.render(); // returns a string representation of the universe and puts it into the JS element
+  universe.tick(); // tells universe to do 1 iteration
+
+  requestAnimationFrame(renderLoop); 
+};
+
+// make first call. Inside our callback it calls it again
+requestAnimationFrame(renderLoop);
+
+
